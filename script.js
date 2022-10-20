499fac51dafbcfaebdeaafec6e6873f06050be91
ARandomGuyYT <116282390+ARandomGuyYT@users.noreply.github.com>
2022-10-20 15:33:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const CFHeading = document.querySelector("#CFHeading");
+const CFButton = document.querySelector("#CFButton");
+
+let cf = 0;
+
+CFButton.addEventListener("click", function () {
+	cf += 1;
+	CFHeading.innerHTML = `CF: ${cf}`;
+});
